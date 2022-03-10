da5de1243e324eb942ef48501b58706e319ec1bf
camilodiaz37 <camilodiaz37@gmail.com>
2022-03-10 03:20:08 -0300

    make sanity interface work

--- packages/studio/schemas/transactionSchema.js
+++ packages/studio/schemas/transactionSchema.js
@@ -1,7 +1,32 @@
 export const transactionSchema = {
-
-
-
-
-
+    name: "transactions",
+    title: 'Transactions',
+    type: "document",
+    fields: [
+        {
+            name: "tHash",
+            title: "Transaction Hash",
+            type: "string",
+        },
+        {
+            name: "fromAddress",
+            title: "From (Wallet Address)",
+            type: "string",
+        },
+        {
+            name: "toAddress",
+            title: "To (Wallet Address)",
+            type: "string",
+        },
+        {
+            name: "amount",
+            title: "Amount",
+            type: "number",
+        },
+        {
+            name: "timeStamp",
+            title: "Timestamp",
+            type: "datetime",
+        }
+    ]
 }
